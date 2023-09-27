c408fea18af7bce95443127a033a00889ccd65fc
Gunjan0311 <104626880+Gunjanmadhwani@users.noreply.github.com>
2023-09-27 21:22:02 +0530

    Delete mini project directory

--- mini project/home page/script.js
+++
@@ -1,9 +0,0 @@
-const menu = document.querySelector(".menu");
-const close = document.querySelector(".close");
-const links = document.querySelector("#navLinks");
-
-function toggleNav() {
-  links.classList.toggle("active");
-}
-menu.addEventListener("click", toggleNav);
-close.addEventListener("click", toggleNav);
